f26bf03235c0460db1f5a97813a3ef374f30c050
CyberSteve99 <cybersteve99@gmail.com>
2025-11-25 22:48:42 +0000

    Improve Install

--- Install/InstallF2Bmqtt.sh
+++ Install/InstallF2Bmqtt.sh
@@ -12,7 +12,7 @@
 echo "🚀    Getting GitHub Archive."
 wget -q https://github.com/CyberSteve99/Fail2BanMQTT/archive/refs/heads/main.zip -O Fail2BanMQTT.zip
 echo "🚀    Inzipping GitHub Archive."
-unzip Fail2BanMQTT.zip
+unzip -qq Fail2BanMQTT.zip
 echo "🚀    Moving Files."
 mv Fail2BanMQTT-main/etc/fail2ban/mqtt.env              /etc/fail2ban/
 mv Fail2BanMQTT-main/etc/fail2ban/mqttsubscribebans     /etc/fail2ban/
@@ -31,7 +31,7 @@ mv /etc/fail2ban/f2bmqttsub.service /etc/systemd/system/f2bmqttsub.service
 systemctl daemon-reload
 systemctl enable f2bmqttsub.service
 systemctl restart f2bmqttsub.service
-echo "🚀    Tidying Upe."
+echo "🚀    Tidying Up."
 rm -r Fail2BanMQTT-main
 rm Fail2BanMQTT.zip
 echo "✅    Complete."
